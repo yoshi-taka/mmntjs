3c450cddb6a7d03efbf57c0937547032cbd27202
yoshi-taka <3677119+yoshi-taka@users.noreply.github.com>
2026-05-26 03:37:55 +0900

    add @astrojs/sitemap integration

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mmntjs.veritycost.com",
+  integrations: [sitemap()],
+});
